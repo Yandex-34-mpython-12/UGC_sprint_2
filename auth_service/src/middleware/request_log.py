3fbf5c5f9c8eb51b893c1d818c4cb4f3154999d5
vladislavbaluta <vladislav.baluta@itglobal.com>
2024-09-24 14:48:30 +1000

    fix nginx logs and log X-request_id

--- auth_service/src/middleware/request_log.py
+++ auth_service/src/middleware/request_log.py
@@ -1,7 +1,10 @@
+import uuid
+
 from fastapi import Request, Response
 from starlette.background import BackgroundTask
 from starlette.middleware.base import BaseHTTPMiddleware
 
+from src.core.context import ctx_request_id
 from src.core.logger import logger
 
 
@@ -20,6 +23,10 @@ def write_log_data(request: Request, response: Response):
 
 class RequestLogMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
+        request_id = request.headers.get('X-Request-Id', str(uuid.uuid4()))
+        ctx_request_id.set(request_id)
+
         response = await call_next(request)
+        response.headers['X-Request-Id'] = request_id
         response.background = BackgroundTask(write_log_data, request, response)
         return response
