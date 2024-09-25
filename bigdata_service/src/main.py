197642f325036668f25c073c0d61fa0f825ee6f1
vladislavbaluta <vladislav.baluta@itglobal.com>
2024-09-25 15:23:32 +1000

    add mongo crud

--- bigdata_service/src/main.py
+++ bigdata_service/src/main.py
@@ -5,10 +5,14 @@ import uvicorn
 from aiokafka import AIOKafkaProducer
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
-from src.api import router as api_router
+from fastapi_pagination import add_pagination
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from src.api import setup_routers
 from src.core.config import settings
 from src.core.logger import LOGGING, setup_root_logger
 from src.middleware.request_log import RequestLogMiddleware
+from src.db import mongo, init_db
 from src.services import kafka
 
 setup_root_logger()
@@ -17,30 +21,40 @@ logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
-    # on_startup
+async def lifespan(_: FastAPI):
     kafka.kafka_producer = AIOKafkaProducer(
         bootstrap_servers=[settings.kafka.bootstrap_server],
     )
     await kafka.kafka_producer.start()
+    mongo.mongo_client = AsyncIOMotorClient(str(settings.mongo.mongodb_uri))
+    await init_db.init(client=mongo.mongo_client)
 
     yield
 
-    # on_shutdown
     await kafka.kafka_producer.stop()
+    mongo.mongo_client.close()
 
-app = FastAPI(
-    title='bigdata_service',
-    docs_url='/api/openapi',
-    openapi_url='/api/openapi.json',
-    default_response_class=ORJSONResponse,
-    root_path="/bigdata",
-    lifespan=lifespan
-)
 
+def create_app():
+    application = FastAPI(
+        lifespan=lifespan,
+        title='bigdata_service',
+        docs_url='/api/openapi',
+        openapi_url='/api/openapi.json',
+        default_response_class=ORJSONResponse,
+        root_path="/bigdata",
+        description='Bigdata Service',
+        version='0.1.0',
+    )
+
+    setup_routers(application)
+    add_pagination(application)
+
+    return application
+
+
+app = create_app()
 app.add_middleware(RequestLogMiddleware)
-
-app.include_router(api_router)
 
 if __name__ == '__main__':
     uvicorn.run(
---
+++ bigdata_service/src/schemas/post.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel, ConfigDict, Field
+
+from src.schemas.author import Author
+
+
+class PostCreateDto(BaseModel):
+    subject: str
+    text: str
+    author: Author
+
+
+class Like(BaseModel):
+    created_at: datetime
+    author: Author
+
+
+class PostResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True, arbitrary_types_allowed=True, populate_by_name=True)
+
+    id: UUID = Field(UUID, validation_alias='_id')
+    slug: str
+    subject: str
+    text: str
+    is_published: bool
+    views: int
+    last_visit_at: datetime | None = None
+    created_at: datetime
+    author: Author
+    likes: list[Like] | None = None
