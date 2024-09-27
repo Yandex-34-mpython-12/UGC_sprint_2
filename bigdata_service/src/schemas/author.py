95ba287f557b5afb53bd0f13a4f85d12e427bfa8
vladislavbaluta <vladislav.baluta@itglobal.com>
2024-09-27 15:29:53 +1000

    change author model

--- bigdata_service/src/schemas/author.py
+++ bigdata_service/src/schemas/author.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Author(BaseModel):
-    first_name: str
-    last_name: str
+    first_name: str = Field(..., min_length=1, max_length=100, description="The author's first name")
+    last_name: str = Field(..., min_length=1, max_length=100, description="The author's last name")
